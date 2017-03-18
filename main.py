31fb132bb929f8b61fddd2c9fbe1a3b685d89d1b
Edwin Chu <edwincheese@gmail.com>
2017-03-18 14:36:47 -0700

    Clean up error and logging

--- main.py
+++ main.py
@@ -4,32 +4,64 @@ import os
 import urllib2
 import wave
 
-from flask import Flask, app, request
+from flask import Flask, app, request, url_for
 from twilio import twiml
-from twilio.rest import TwilioRestClient
 from google.cloud import speech
+
+
+TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
 
 
 app = Flask(__name__)
 
 
-def _fetch_sample(speech_client, recording_url):
-    content = urllib2.urlopen(recording_url)
-    wav = wave.open(content, 'r')
-    encoding = 'LINEAR16'
-    sample_rate = wav.getframerate()
-    frames = wav.readframes(wav.getnframes())
-    return speech_client.sample(encoding=encoding, sample_rate=sample_rate, content=frames)
+def _recognize(recording_url):
+    content = None
+    wav = None
+    try:
+        content = urllib2.urlopen(recording_url)
+        wav = wave.open(content, 'r')
+        encoding = 'LINEAR16'
+        sample_rate = wav.getframerate()
+        frames = wav.readframes(wav.getnframes())
+
+        client = speech.Client()
+        sample = client.sample(encoding=encoding, sample_rate=sample_rate, content=frames)
+        results = sample.sync_recognize(max_alternatives=1, language_code='zh-HK')
+        app.logger.info("even=recognize_success recording_url=%s transcript=%s confidence=%f", recording_url, results[0].transcript, results[0].confidence)
+        return results[0].transcript
+    except Exception as e:
+        app.logger.info("evt=recognize_fail sid=%s recording_url=%s err=%s", request.form['CallSid'], recording_url, e)
+        raise
+    finally:
+        if content != None:
+            content.close()
+        if wav != None:
+            wav.close()
+
+
+def _delete_recording(recording_url):
+    sid = recording_url[recording_url.rindex('/') + 1:]
+    client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    client.recordings.delete(sid)
+    logging.debug("evt=recording_deleted sid=%s", sid)
+
+
+def _say(r, message):
+    r.say(message, voice='alice', language='zh-HK')
 
 
 @app.route('/hello', methods=['POST', 'GET'])
 def hello():
-    app.logger.info("action=hello")
-
     r = twiml.Response()
-    r.say(u'歡迎致電前線科技人員，真 Connect。請讀出你既區議會分區', voice='alice', language='zh-HK')
-    r.record(action="/recognize", maxLength=20, playBeep=False)
-    r.redirect("/hello", method='POST')
+    _say(r, u'歡迎致電前線科技人員，真 Connect。請讀出你既區議會分區')
+    r.record(action=url_for('recognize'), maxLength=10, playBeep=False, timeout=3)
+    _say(r, u'請讀出你既區議會分區')
+    r.record(action=url_for('recognize'), maxLength=10, playBeep=False, timeout=3)
+    r.hangup()
+
+    app.logger.info("evt=hello sid=%s from=%s", request.form['CallSid'], request.form['From'])
 
     return str(r), 200, {'Content-Type': 'text/xml'}
 
@@ -37,22 +69,15 @@ def hello():
 @app.route('/recognize', methods=['POST', 'GET'])
 def recognize():
     recording_url = request.form['RecordingUrl']
-    app.logger.info("action=recognize recording_url=%s", recording_url)
 
     r = twiml.Response()
-    client = speech.Client()
     try:
-        sample = _fetch_sample(client, recording_url)
-        results = sample.sync_recognize(max_alternatives=1, language_code='zh-HK')
-        app.logger.info("action=recognize transcript=%s confidence=%f", results[0].transcript, results[0].confidence)
-        result = results[0].transcript
-        r.say(u'你講既係' + result, voice='alice', language='zh-HK')
+        result = _recognize(recording_url)
+        _say(r, u'你講既係' + result)
         r.hangup()
     except ValueError:
-        r.say(u'唔好意思，我唔係好知你講乜野，請試多次。', voice='alice', language='zh-HK')
-        r.record(action="/recognize", maxLength=20, playBeep=False)
-        r.say(u'唔好意思，請遲啲再打過黎啦。', voice='alice', language='zh-HK')
-        r.hangup()
+        _say(r, u'我唔係好知你講乜野，請試多次。')
+        r.redirect(url_for('hello', intro=False))
 
     return str(r), 200, {'Content-Type': 'text/xml'}
 
@@ -60,13 +85,10 @@ def recognize():
 @app.errorhandler(500)
 def server_error(e):
     # Log the error and stacktrace.
-    logging.exception('An error occurred during a request.')
-    r.say
+    logging.exception('evt=error err=%s', e)
     r = twiml.Response()
-
-    r.say(u'唔好意思，我地發生咗錯誤，請遲啲再打過黎啦。', voice='alice', language='zh-HK')
+    _say(r, u'唔好意思，系統發生咗啲故障，請遲啲再打過黎啦。')
     r.hangup()
-
     return str(r), 200, {'Content-Type': 'text/xml'}
 
 
