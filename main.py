4cbb13aa289448185bf904dd230f73655fb74d0d
Edwin Chu <edwincheese@gmail.com>
2017-03-18 18:47:10 -0700

    Respond to user immediately after receiving input

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def _lookup_district(text):
 def hello():
     r = twiml.Response()
     _say(r, u'你好！呢度係前線科技人員設立嘅 真 WeConnect 熱線。請講出你喺18區入面，係屬於邊一區，或者講一個區議員嘅名字。')
-    r.record(action=url_for('recognize'), maxLength=10, playBeep=False, timeout=3)
+    r.record(action=url_for('accept'), maxLength=10, playBeep=True, timeout=2)
     r.redirect(url_for('retry'))
     app.logger.info("evt=hello sid=%s from=%s", request.form['CallSid'], request.form['From'])
 
@@ -121,7 +121,7 @@ def hello():
 def retry():
     r = twiml.Response()
     _say(r, u'請講出你區名，或者一個區議員嘅名。')
-    r.record(action=url_for('recognize'), maxLength=10, playBeep=False, timeout=3)
+    r.record(action=url_for('accept'), maxLength=10, playBeep=True, timeout=2)
     r.hangup()
 
     app.logger.info("evt=retry sid=%s", request.form['CallSid'])
@@ -129,9 +129,24 @@ def retry():
     return str(r), 200, {'Content-Type': 'text/xml'}
 
 
+@app.route('/accept', methods=['POST', 'GET'])
+def accept():
+    recording_url = request.form['RecordingUrl']
+    r = twiml.Response()
+    _say(r, u'收到。')
+    r.redirect(url_for('recognize', RecordingUrl=recording_url))
+
+    app.logger.info("evt=accept sid=%s", request.form['CallSid'])
+
+    return str(r), 200, {'Content-Type': 'text/xml'}
+
+
 @app.route('/recognize', methods=['POST', 'GET'])
 def recognize():
-    recording_url = request.form['RecordingUrl']
+    if 'RecordingUrl' in request.args:
+        recording_url = request.args['RecordingUrl']
+    else:
+        recording_url = request.form['RecordingUrl']
 
     r = twiml.Response()
     try:
@@ -140,10 +155,10 @@ def recognize():
         district_matches = _lookup_district(text)
         name = None
         if len(name_matches) > 0:
-            _say(r, u'收到。而家我會幫你打比')
+            _say(r, u'。而家我會幫你打比')
             name = name_matches[0]
         elif len(district_matches) > 0:
-            _say(r, u'收到。而家我會幫你打比其中一位屬於特首選委既')
+            _say(r, u'而家我會幫你打比其中一位屬於特首選委既')
             _say(r, district_matches[0])
             _say(r, u'區議員')
             name = random.choice(district_dir[district_matches[0]])
