9bd0e8205ef2bccc72156afe186641559cacd619
rickehor <rickehor@Rickes-Mac-mini.local>
2017-03-21 20:06:48 +0800

    added EC members who are also DC and LegCo members

--- csv_to_yaml.sh
+++ csv_to_yaml.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-awk -F, '{printf "\047%s\047:\n  district: \047%s\047\n  desc: \047%s%s%s\047\n  tel: \[\047%s\047\]\n  email: \[\047%s\047\]\n  addr: \[\047%s\047\]\n  district_alias: \[\047%s\047\]\n", $1, $2, $6, $7, $1, $3, $4, $5, $8}' DC_EC_Members.csv | sed -e "s/\//','/g" 
+awk -F, '{printf "\047%s\047:\n  district: \047%s\047\n  desc: \047%s%s%s\047\n  tel: \[\047%s\047\]\n  email: \[\047%s\047\]\n  addr: \[\047%s\047\]\n", $1, $2, $6, $7, $1, $3, $4, $5}' DC_EC_Members.csv | sed -e "s/\//','/g" 
